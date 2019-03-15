f82a0a4b21e69416ff438c48907e6d5933f27720
Thomas Morris <tmorris@salsitasoft.com>
2019-03-15 15:51:23 +0100

    Remove unneeded header

---
+++ src/components/WebsiteList/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class WebsiteList extends React.Component {
+  render() {
+    return ();
+  }
+}
+
+WebsiteList.propTypes = {
+
+};
+
+export default WebsiteList;
